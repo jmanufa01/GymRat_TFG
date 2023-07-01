c7bec4237faed399269ec96d8951463220ca76cc
jmanufa01 <75038141+jmanufa01@users.noreply.github.com>
2023-07-01 12:48:27 +0200

    GymRat App BackEnd created

---
+++ BackEnd_GymRat/src/main/java/com/example/backend_gymrat/BackEndGymRatApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_gymrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndGymRatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndGymRatApplication.class, args);
+    }
+
+}
---
+++ BackEnd_GymRat/src/test/java/com/example/backend_gymrat/BackEndGymRatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_gymrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndGymRatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
