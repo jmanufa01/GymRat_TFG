784aab1907d4c16cb4abb00d3a935db9eef3da66
jmanufa01 <75038141+jmanufa01@users.noreply.github.com>
2023-07-12 19:22:40 +0200

    Created authentication routing

--- GymRat_Front/src/app/app-routing.module.ts
+++ GymRat_Front/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/pages/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'error',
+    component: ErrorPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- GymRat_Front/src/app/auth/auth.module.ts
+++ GymRat_Front/src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent,
+    RegisterPageComponent,
+    AuthLayoutComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
